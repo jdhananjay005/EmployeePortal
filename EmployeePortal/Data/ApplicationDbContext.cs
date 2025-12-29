3b535dc3c2ba34b71d064af2055baf2045416640
Dhananjay <jdhananjay005@gmail.com>
2025-12-29 20:21:20 +0530

    Created the Database. Established the connections with SQL.

---
+++ EmployeePortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeePortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
